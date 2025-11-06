3e39aa27a251a42fa9baa169373af35227db1853
SajibSaha207 <sajibsaha.207@gmail.com>
2025-11-07 03:27:16 +0600

    added hero swiper

--- src/Components/HeroSlider.jsx
+++ src/Components/HeroSlider.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css'; 
+import H1 from '../assets/img1.jfif'
+import H2 from '../assets/download.jfif'
+import H3 from '../assets/img 3.jfif'
+import H4 from '../assets/img 4.jfif'
+import H5 from '../assets/img 5.jfif'
+import H6 from '../assets/img 6.jfif'
 const HeroSlider = () => {
     return (
-        <div className="hero bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl"
-    />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
+        <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        modules={[Autoplay]}
+        autoplay={{ delay: 1500, disableOnInteraction: false }}
+        loop={true} 
+        onSlideChange={() => console.log()}
+        onSwiper={(swiper) => console.log()}
+      >
+        <SwiperSlide><img src={H1} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+        <SwiperSlide><img src={H2} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+        <SwiperSlide><img src={H3} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+        <SwiperSlide><img src={H4} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+        <SwiperSlide><img src={H5} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+        <SwiperSlide><img src={H6} alt="" className='w-full h-[250px] object-cover' /></SwiperSlide>
+      </Swiper>
     </div>
-  </div>
-</div>
-    );
+  );
 };
-
 export default HeroSlider;
