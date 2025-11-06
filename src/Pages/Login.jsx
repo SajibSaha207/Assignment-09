beb66f0b5e64ca3c975c4adc80f5ed2dbec008b3
SajibSaha207 <sajibsaha.207@gmail.com>
2025-11-07 00:20:01 +0600

    added navigate to home page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,31 @@
-import { sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { GoogleAuthProvider, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import React, { useRef, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { auth } from '../Firebase/Firebase.config';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
+
+const googleProvider = new GoogleAuthProvider();
 const Login = () => {
+  const navigate = useNavigate();
+
+  //google login
+
+const handleGoogleSignIn = ()=>{
+  // console.log('google button click')
+  signInWithPopup(auth, googleProvider)
+  .then((result)=>{
+      navigate("/", {replace: true});
+    console.log(result)
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+
+}
+
+
+
   const [error, setError] = useState('');
   const emailRef = useRef();
    const [showPassword, setShowPassword] = useState(false)
@@ -20,6 +41,7 @@ setError('');
 
  signInWithEmailAndPassword(auth, email, password)
  .then(result =>{
+  navigate("/", {replace: true});
   console.log(result.user)
  })
  .catch(error => {
@@ -69,7 +91,7 @@ const handleTooglePasswordShow = (event)=>{
           
           <div onClick={handleForgatePassword}><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Login</button>
-          <button className="btn bg-white text-black border-[#e5e5e5]">
+          <button onClick={handleGoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,11 +1,34 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { auth } from '../Firebase/Firebase.config';
 import Swal from "sweetalert2";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
+
+const googleProvider = new GoogleAuthProvider();
 const SignUp = () => {
+
+  const navigate = useNavigate();
+
+  //google login
+
+const handleGoogleSignIn = ()=>{
+  // console.log('google button click')
+  signInWithPopup(auth, googleProvider)
+  .then((result)=>{
+      navigate("/", {replace: true});
+    console.log(result)
+  })
+  .catch(error =>{
+    console.log(error)
+  })
+
+}
+
+
+
+
   const [error, setError] = useState('')
   const [success, setSucess] = useState('')
   const [showPassword, setShowPassword] = useState(false)
@@ -42,6 +65,7 @@ setSucess(false);
   //Create user with email and pass
   createUserWithEmailAndPassword(auth,email,password)
   .then(result =>{
+    navigate("/", {replace: true});
  console.log('after create user',result.user)
  setSucess(true);
  e.target.reset();
@@ -98,7 +122,7 @@ const handleTooglePasswordShow = (event)=>{
        
  
           <button className="btn btn-neutral mt-4">Sign Up</button>
-          <button className="btn bg-white text-black border-[#e5e5e5]">
+          <button onClick={handleGoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
